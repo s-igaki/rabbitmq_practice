c781043b8d94e797f15cf8889a95d71816cdf648
igakiShusei <igakishusei@team-lab.com>
2021-08-12 13:36:39 +0900

    複数メッセージを送信するように変更した

--- producer_main.py
+++ producer_main.py
@@ -1,6 +1,8 @@
 #!/usr/local/var/pyenv/shims/python3
 
 import pika
+import datetime
+import json
 
 def connectPika():
     # コネクション作成
@@ -12,9 +14,23 @@ def connectPika():
 
     # キューの作成
     channel.queue_declare(queue='hello')
+    properties = pika.BasicProperties(content_type='application/json')
 
     # メッセージの送信
-    channel.basic_publish(exchange="", routing_key="hello", body="hello wolrd!")
+    dt_now = datetime.datetime.now()
+    channel.basic_publish(exchange="", routing_key="hello", body=json.dumps({'title':"1hello wolrd {}!".format(dt_now)}), properties=properties)
+
+    dt_now = datetime.datetime.now()
+    channel.basic_publish(exchange="", routing_key="hello", body=json.dumps({'title':"2hello wolrd {}!".format(dt_now)}), properties=properties)
+
+    dt_now = datetime.datetime.now()
+    channel.basic_publish(exchange="", routing_key="hello", body=json.dumps({'title':"3hello wolrd {}!".format(dt_now)}), properties=properties)
+
+    dt_now = datetime.datetime.now()
+    channel.basic_publish(exchange="", routing_key="hello", body=json.dumps({'title':"4hello wolrd {}!".format(dt_now)}), properties=properties)
+
+    dt_now = datetime.datetime.now()
+    channel.basic_publish(exchange="", routing_key="hello", body=json.dumps({'title':"5hello wolrd {}!".format(dt_now)}), properties=properties)
 
     # コネクション終了
     connection.close()
